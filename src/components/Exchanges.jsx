b509ea741f8cb37f9522a87fcdc6e8594e507c4f
Ishank Chopra <ishankchopranbd@gmail.com>
2023-12-12 23:39:05 +0530

    Set API

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { server } from "../index";
+import { Container, HStack } from "@chakra-ui/react";
+import Loader from "./Loader";
 
 const Exchanges = () => {
-  return <div>I am exchanges</div>;
+  const [exchanges, setExchanges] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get(`${server}/exchanges`).then((response) => {
+      console.log(response.data);
+      setExchanges(response.data);
+      setLoading(false);
+    });
+  }, []);
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <HStack>
+            {exchanges.map((i) => {
+              return <div>{i.name}</div>;
+            })}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default Exchanges;
