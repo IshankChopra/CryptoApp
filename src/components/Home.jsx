279817384224069d8ae4f846e91c61f5c6972210
Ishank Chopra <ishankchopranbd@gmail.com>
2023-12-17 01:49:08 +0530

    Make Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,29 @@
+import { Box, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import image from "../assets/BitCoin.png";
 
 const Home = () => {
-  return <div>I am Home</div>;
+  return (
+    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
+      <Image
+        w={"full"}
+        h={"full"}
+        objectFit={"contain"}
+        src={image}
+        //filter={"grayscale(1)"}
+      />
+
+      <Text
+        fontSize={"6xl"}
+        textAlign={"center"}
+        fontWeight={"thin"}
+        color={"whiteAlpha.700"}
+        mt={"-20"}
+      >
+        XCrypto
+      </Text>
+    </Box>
+  );
 };
 
 export default Home;
