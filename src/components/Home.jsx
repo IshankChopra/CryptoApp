fd4603d8a7ac7b0949056681ebe6f296341b11bc
Ishank Chopra <ishankchopranbd@gmail.com>
2023-12-18 00:11:28 +0530

    Deploy Successfully on Vercel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,6 @@ import image from "../assets/BitCoin.png";
 const Home = () => {
   return (
     <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
-      <Image
-        w={"full"}
-        h={"full"}
-        objectFit={"contain"}
-        src={image}
-        //filter={"grayscale(1)"}
-      />
-
       <Text
         fontSize={"6xl"}
         textAlign={"center"}
@@ -20,8 +12,15 @@ const Home = () => {
         color={"whiteAlpha.700"}
         mt={"-20"}
       >
-        XCrypto
+        CryptoSphere
       </Text>
+      <Image
+        w={"full"}
+        h={"full"}
+        objectFit={"contain"}
+        src={image}
+        //filter={"grayscale(1)"}
+      />
     </Box>
   );
 };
